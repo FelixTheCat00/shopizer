4ab5157b4886794105a34896602b1f9878b79bfe
FelixTheCat00 <felix.mensahadjei@gmail.com>
2019-10-04 15:58:54 +0200

    First commit

---
+++ shopizer/src/main/java/shopizer/shopizer_art/App.java
@@ -0,0 +1,13 @@
+package shopizer.shopizer_art;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
